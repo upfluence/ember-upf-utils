12c7e69dbe1896e85450313e956b7eae7a97f0f5
Julien Levesy <jlevesy@gmail.com>
2016-09-15 16:07:27 +0200

    factorize errors-list component, use it in new account form

---
+++ tests/integration/components/errors-list-test.js
@@ -0,0 +1,9 @@
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('errors-list', 'Integration | Component | errors list', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(0);
+});
