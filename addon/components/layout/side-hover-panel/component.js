fe00dd6d8018315df739a68f2fe746fd8476c8f8
aguado_e <enzo.aguado@epitech.eu>
2017-01-27 17:27:04 +0100

    clean some code

--- addon/components/layout/side-hover-panel/component.js
+++ addon/components/layout/side-hover-panel/component.js
@@ -9,26 +9,29 @@ export default Ember.Component.extend({
   // Those are the parameters that you can override
   backdropAction: null,
   side: "right",
-  shouldAnimate: "true",
+  shouldAnimate: true,
   width: "100%",
   height: "100%",
-  disableScrolling: "false",
+  disableScrolling: false,
   stickTo: "right",
 
   didInsertElement() {
     // limit the amount of work for the brower
     this.$().detach().prependTo($(document.body));
-    this.$('.hover-panel').addClass(this.get('side') + "_side");
-    this.$('.hover-panel').addClass(this.get('stickTo') + "_align");
 
-    // Set animations
-    if (this.get('shouldAnimate') === "true")
-      this.$('.hover-panel')
-        .css('transition', 'transform .35s cubic-bezier(.25,.8,.25,1)');
+
+    let hover_panel = this.$('.hover-panel');
+
+    hover_panel.addClass(this.get('side') + "_side");
+    hover_panel.addClass(this.get('stickTo') + "_align");
+
+    // Set animationsaddClass
+    if (this.get('shouldAnimate') === true)
+      hover_panel.addClass('animate');
 
     // Insert panel
     Ember.run.later(() => {
-      this.$('.hover-panel').addClass(this.get('side') + "_transform");
+      hover_panel.addClass(this.get('side') + "_transform");
     });
 
     // Insert backdrop
@@ -36,28 +39,27 @@ export default Ember.Component.extend({
       this.$('.panel-backdrop').removeClass('hidden');
 
     // Set panel dimensions
-    this.$('.hover-panel').css('width', this.get('width'));
-    this.$('.hover-panel').css('height', this.get('height'));
+    hover_panel.css({
+        'width': this.get('width'),
+        'height': this.get('height')
+    });
 
     // Disable scrolling
-    if (this.get("disableScrolling") === "true") {
-      $('body').css('height', '100%');
-      $('body').css('overflow', 'hidden');
+    if (this.get("disableScrolling") === true) {
+      $('body').addClass('disable-scrolling');
     }
   },
 
   willDestroyElement() {
     // Re-enable scrolling
-    if (this.get("disableScrolling") === "true")
-    {
-      $('body').css('height', 'auto');
-      $('body').css('overflow', 'auto');
+    if (this.get("disableScrolling") === true) {
+      $('body').removeClass('prevent-scrolling');
     }
   },
 
   actions: {
-    backdropAction() {
-      this.get('backdropAction')();
+    sendBackdropAction() {
+      this.sendAction('backdropAction');
     }
   }
 });
