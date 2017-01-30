bd8812a58b72dbbc399ccc14a74be93013239d3e
tgallice <tmgallice@gmail.com>
2017-01-30 21:14:47 +0100

    init default 'value'

--- addon/components/toggle-area/component.js
+++ addon/components/toggle-area/component.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
   layout,
   classNames: ['__toggle-area'],
   classNameBindings: ['value:toggled'],
+  value: false,
 
   click() {
     this.toggleProperty('value');
