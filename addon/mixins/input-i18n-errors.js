634272d13ca98fabcb1a76a1d31afaf70cf5943d
Philippe Ndiaye <phndiaye@gmail.com>
2017-03-03 14:05:43 +0100

    forgot fetch observer from Ember

--- addon/mixins/input-i18n-errors.js
+++ addon/mixins/input-i18n-errors.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 import { translationMacro as t } from 'ember-i18n';
 
-const { Mixin } = Ember;
+const { Mixin, observer } = Ember;
 
 export default Mixin.create({
   i18n: Ember.inject.service(),
+
   _: observer('_error', function() {
     this.set('error', t(this.get('_error')));
-  }),
+  })
 });
