ffb7e8b97558d11f0bf9467665e65701a874b7db
Philippe Ndiaye <phndiaye@gmail.com>
2017-03-03 14:03:39 +0100

    added a mixin for adding i18n support to an input-wrapper

---
+++ addon/mixins/input-i18n-errors.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+import { translationMacro as t } from 'ember-i18n';
+
+const { Mixin } = Ember;
+
+export default Mixin.create({
+  i18n: Ember.inject.service(),
+  _: observer('_error', function() {
+    this.set('error', t(this.get('_error')));
+  }),
+});
