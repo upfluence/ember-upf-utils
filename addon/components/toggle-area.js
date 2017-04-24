9004b0f96e43fae71270b60208bf22251699e747
Philippe Ndiaye <phndiaye@gmail.com>
2017-04-24 23:03:49 +0200

    allow the toggle-area component to take callback actions

--- addon/components/toggle-area.js
+++ addon/components/toggle-area.js
@@ -9,6 +9,17 @@ export default Component.extend({
 
   click() {
     this.toggleProperty('value');
+
+    if(this.get('callbackAction')) {
+      this.send('runCallback');
+    }
+
     return false; // Don't propagate the event any further
+  },
+
+  actions: {
+    runCallback: function() {
+      this.get('_targetObject').send(this.get('callbackAction'));
+    }
   }
 });
