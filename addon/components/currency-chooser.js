0ba48d59628fc8ae0ea72038db9e7f736695a639
Alexis Montagne <alexis.montagne@gmail.com>
2017-05-03 16:07:35 -0400

    Fix the currency-chooser

--- addon/components/currency-chooser.js
+++ addon/components/currency-chooser.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 import layout from '../templates/components/currency-chooser';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+
+  actions: {
+    updateCurrency(currency) {
+      this.set('value', currency);
+    }
+  }
 });
