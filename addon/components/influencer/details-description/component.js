ab0cc7f9a22ee38a5682b4b26e712cd177d5690c
tgallice <tmgallice@gmail.com>
2017-05-04 17:35:50 +0200

    Add link to influencer in description

--- addon/components/influencer/details-description/component.js
+++ addon/components/influencer/details-description/component.js
@@ -4,6 +4,17 @@ import layout from './template';
 export default Ember.Component.extend({
   layout,
   classNames: ['details-description'],
+  displayLink: false,
+
+  facadeURL: Ember.computed(function() {
+    return Ember.getOwner(this).resolveRegistration(
+      'config:environment'
+    ).facadeURL;
+  }),
+
+  link: Ember.computed('facadeURL', 'profile.id', function() {
+    return `${this.get('facadeURL')}influencers/${this.get('profile.id')}`
+  }),
 
   actions: {
     close() {
