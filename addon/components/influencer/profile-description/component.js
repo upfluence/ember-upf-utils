80190beec655437705f2278976b1294fab3a69b6
tgallice <tmgallice@gmail.com>
2017-05-05 10:35:32 +0200

    Make profile-description more modulable

--- addon/components/influencer/profile-description/component.js
+++ addon/components/influencer/profile-description/component.js
@@ -4,5 +4,24 @@ import layout from './template';
 export default Ember.Component.extend({
   layout,
   classNames: ['profile-description'],
-  isSelectable: true
+  isSelectable: true,
+  toggleAction: null,
+  selected: null,
+
+  // Avoid BC break with profile.selected
+  _selected: Ember.computed('selected', 'profile.selected', {
+    get() {
+      return this.get('selected') || this.get('profile.selected');
+    },
+    set(_, value) {
+      //selected not set
+      if (this.get('selected') === null) {
+        this.set('profile.selected', value);
+      } else {
+        this.set('selected', value);
+      }
+
+      return value;
+    }
+  })
 });
