1d683bfc1482b458f4427bbaf7b6cb1e835d3cd8
tgallice <tm.gallice@gmail.com>
2017-07-10 11:45:46 +0200

    Add error renderer

---
+++ app/helpers/error-renderer.js
@@ -0,0 +1,2 @@
+import ErrorRenderer from 'ember-upf-utils/helpers/error-renderer';
+export default ErrorRenderer;
---
+++ tests/unit/helpers/display-error-test.js
@@ -0,0 +1,12 @@
+
+import { displayError } from 'dummy/helpers/display-error';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | display error');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = displayError([42]);
+  assert.ok(result);
+});
+
