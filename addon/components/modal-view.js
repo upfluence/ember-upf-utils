9f9bc057e4878b17819a056c31dbd01e76dd791c
Toon van Ramshorst <ramshorst@gmail.com>
2017-08-14 15:46:51 +0200

    Clean focussing + keyboard support

--- addon/components/modal-view.js
+++ addon/components/modal-view.js
@@ -4,9 +4,11 @@ import layout from '../templates/components/modal-view';
 export default Ember.Component.extend({
   layout,
   classNames: ['modal', 'fade'],
+  attributeBindings: ['tabindex'],
+  tabindex: -1,
 
   didInsertElement() {
-    this.$(this.element).modal({keyboard: true, backdrop: 'static'});
+    this.$().modal({keyboard: true, backdrop: 'static'});
   },
   willDestroyElement() {
     this.$(this.element).modal('hide');
