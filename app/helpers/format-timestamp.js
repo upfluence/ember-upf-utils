37b0cf269a24bbc03d3f75ac5078e7a58ddad32b
Toon van Ramshorst <ramshorst@gmail.com>
2017-09-27 15:43:27 +0200

    Adding timestamp helper

---
+++ app/helpers/format-timestamp.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import moment from 'moment';
+
+let formatTimestamp = timestamp => moment.unix(timestamp).calendar();
+
+let FormatTimestampHelper = Ember.Helper.helper(formatTimestamp);
+
+export { formatTimestamp };
+
+export default FormatTimestampHelper;
