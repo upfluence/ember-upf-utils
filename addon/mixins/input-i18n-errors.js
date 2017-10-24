78752717193d4bd2c82664ce5c21f6ba29e40251
Philippe Ndiaye <phndiaye@gmail.com>
2017-10-24 16:42:42 +0200

    fix i18n unused

--- addon/mixins/input-i18n-errors.js
+++ addon/mixins/input-i18n-errors.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import { translationMacro as t } from 'ember-i18n';
 
-const { Mixin, observer } = Ember;
+const {
+  Mixin,
+  inject,
+  observer
+} = Ember;
 
 export default Mixin.create({
-  i18n: Ember.inject.service(),
-
   _: observer('_error', function() {
     this.set('error', t(this.get('_error')));
   })
