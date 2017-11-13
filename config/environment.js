73fc25746323716d4addbb855fbb05052535ec78
Toon van Ramshorst <ramshorst@gmail.com>
2017-11-13 14:11:23 +0100

    Adding default Toastr config

--- config/environment.js
+++ config/environment.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return { modulePrefix: 'ember-upf-utils', };
+  return {
+    modulePrefix: 'ember-upf-utils',
+
+    'ember-toastr': {
+      toastrOptions: {
+        positionClass: 'toast-bottom-center upf-toastr--container',
+        hideDuration: 200,
+        timeOut: 10000,
+        newestOnTop: true
+      }
+    },
+  };
 };
