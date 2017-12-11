a94650a5c4bc13de71bcef1afc14cfc62d336355
Toon van Ramshorst <ramshorst@gmail.com>
2017-12-11 16:27:27 +0000

    Simplify

--- addon/helpers/percentage.js
+++ addon/helpers/percentage.js
@@ -5,8 +5,11 @@ const {
 } = Ember;
 
 export function percentage(input) {
-  let percentage = Math.round((input[0] / input[1]) * 100);
-  return (!isNaN(parseFloat(percentage)) && isFinite(percentage)) ? percentage : '— ';
+  if (!parseInt(input[1])) {
+    return 0;
+  }
+
+  return Math.round((input[0] / input[1]) * 100);
 }
 
 export default Helper.helper(percentage);
