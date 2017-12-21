71bce7cdcdfa6cdeee067d8ced3f81ea446bd050
Philippe Ndiaye <phndiaye@gmail.com>
2017-12-21 14:57:34 +0100

    a service for bulk archiving/unarchiving entities (lists, mailings, etc.)

---
+++ addon/entity-archiving/service.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+const {
+  Service,
+  inject
+} = Ember;
+
+export default Service.extend({
+  ajax: inject.service(),
+  store: inject.service(),
+
+  _perform(method, endpoint, data) {
+    return new Ember.RSVP.Promise((resolve,reject) => {
+      this.get('ajax').request(endpoint, {
+        method: method,
+        contentType: 'application/json',
+        data: JSON.stringify(data)
+      }).then((payload) => {
+        this.get('store').pushPayload(payload);
+        resolve();
+      }).catch((e) => reject(e));
+    });
+  },
+
+  bulkToggleArchive(entityName, entitiesIds, archivalAction) {
+    let params = { archival_action: archivalAction };
+    params[`${entityName}`] = entitiesIds;
+    return this._perform('PUT', `/${entityName}/bulk-toggle-archive`, params);
+  }
+});
