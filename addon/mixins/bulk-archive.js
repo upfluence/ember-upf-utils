a94748aa16bb17791e7120016723fff81e9807ca
Philippe Ndiaye <phndiaye@gmail.com>
2017-12-21 15:09:27 +0100

    a mixin to add bulk archiving to features

---
+++ addon/mixins/bulk-archive.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+const { Mixin } = Ember;
+
+export default Mixin.create({
+  toast: inject.service(),
+  i18n: inject.service(),
+
+  _toastConfig: {
+    timeOut: 0,
+    extendedTimeOut: 0,
+    closeButton: true,
+    tapToDismiss: true
+  },
+
+  actions: {
+    bulkArchive(archivalAction) {
+      this.get('entityArchiving').bulkToggleArchive(
+        this.get('entityArchivingName'),
+        this.get('selectedItems').mapBy('id'),
+        archivalAction
+      ).then(() => {
+        this.get('selectedItems').map((item) => item.set('selected', false));
+        this.get('collection').removeObjects(this.get('selectedItems'));
+      }).catch(() => {
+        this.get('toast').error(
+          this.get('i18n').t(this.get('archivalError')),
+          this.get('i18n').t(this.get('archivalErrorTitle')),
+          this._toastConfig
+        );
+      });
+    },
+  }
+});
---
+++ app/mixins/bulk-archive.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-upf-utils/mixins/bulk-archive';
