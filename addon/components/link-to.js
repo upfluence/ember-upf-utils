628db8c7a3cb23a913ec0be693c7b8f45f942d38
Philippe Ndiaye <phndiaye@gmail.com>
2017-12-29 16:31:19 +0100

    autoload tooltip on link-tos

--- addon/components/link-to.js
+++ addon/components/link-to.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import TooltipActivationMixin from 'ember-upf-utils/mixins/tooltip-activation';
 
 const { LinkComponent } = Ember;
 
-export default LinkComponent.reopen({
+export default LinkComponent.reopen(TooltipActivationMixin, {
   attributeBindings: ['data-toggle', 'data-placement']
 });
