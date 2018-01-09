021e9fa4fe7c02161bde4e13638af43ca78f036f
Philippe Ndiaye <phndiaye@gmail.com>
2018-01-09 12:07:36 +0100

    remove old legacy stuffs

--- addon/components/layout/two-columns/panel-content-left/component.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-import layout from './template';
-
-export default Ember.Component.extend({
-  layout,
-  classNames: ['__panel-content-left'],
-});
--- app/components/layout/two-columns/panel-content-left/title/component.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-upf-utils/components/layout/two-columns/panel-content-left/title/component';
