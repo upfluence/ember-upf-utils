47edee10bea896741d02fd412842602f745b0c23
Philippe Ndiaye <phndiaye@gmail.com>
2018-01-10 22:02:53 +0100

    move the list export under the external-export banner

--- addon/components/universal-export/list/component.js
+++ addon/components/universal-export/list/component.js
@@ -1,36 +1,12 @@
 /*globals ga*/
-import Ember from 'ember';
-import layout from './template';
+import ExternalExport from 'ember-upf-utils/components/universal-export/external/component';
 
-const { Component, computed, inject, isBlank } = Ember;
 
-export default Component.extend({
-  layout,
-  exports: inject.service(),
+export default ExternalExport.extend({
+  layoutName: 'components/universal-export/external',
 
-  disabledExport: computed('currentList', function() {
-    return isBlank(this.get('currentList'));
-  }),
+  _model: 'list',
 
-  items: computed(function() {
-    return this.get('exports').fetchEntities('list', (items) => {
-      return items;
-    });
-  }),
-
-  actions: {
-    closeModal() {
-      ga('send', 'event', 'Header', 'Submit', 'Cancel');
-      this.sendAction('closeModal');
-    },
-
-    submit(params, defer) {
-      let list = params[0];
-
-      this.triggerAction({
-        action: 'performExport',
-        actionContext: [`list:${list.id}`, defer]
-      });
-    }
-  }
+  placeholder: 'Pick or create a list',
+  createOptionPlaceholder: 'Create <strong>#item#</strong> list'
 });
