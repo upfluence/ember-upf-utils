13ec8ce651745f5d39ab5fbe679594c189bb4cb9
tgallice <tmgallice@gmail.com>
2018-01-17 18:14:28 +0100

    Add extract-errors utils

---
+++ addon/utils/errors-extractor.js
@@ -0,0 +1,16 @@
+export default function (payload, i18n) {
+  let errors = [];
+
+  (payload.errors || []).forEach((e) => {
+    if (typeof e === 'string') {
+      errors.push(e)
+    }else if (e.code && e.field && e.resource) {
+      errors.push(i18n.t(
+        `${e.resource}.${e.field}.${e.code}`
+      ))
+    }
+  });
+
+  return errors;
+}
+
