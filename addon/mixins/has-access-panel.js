1cd936460c8748a43092e9e1c3f00a94f67515bd
Philippe Ndiaye <phndiaye@gmail.com>
2018-01-23 16:41:32 +0100

    factorize the access panel support into a mixin

---
+++ addon/mixins/has-access-panel.js
@@ -0,0 +1,61 @@
+import Ember from 'ember';
+import UpfTableSearchMixin from 'oss-components/mixins/upf-table-search';
+
+const {
+  Mixin,
+  computed,
+  observer
+} = Ember;
+
+export default Mixin.create(UpfTableSearchMixin, {
+  displayArchived: false,
+  displayAccessPanel: false,
+  searchCollection: 'accessPanelEntities',
+  searchAttribute: 'name',
+
+  init() {
+    [
+      `${this.get('accessPanelConfig.model')}.@each.archived`,
+      'displayAccessPanel',
+      'displayArchived'
+    ].forEach((property) => {
+      this.addObserver(property, this, this.populateAccessPanel);
+    });
+
+    this._super();
+  },
+
+  populateAccessPanel() {
+    this.set(
+      'accessPanelEntities',
+      this.get(this.get('accessPanelConfig.model')).filterBy(
+        'archived',
+        this.get('displayArchived')
+      )
+    );
+  },
+
+  actions: {
+    toggleAccessPanel() {
+      this.toggleProperty('displayAccessPanel');
+    },
+
+    toggleDisplayArchived() {
+      this.toggleProperty('displayArchived');
+    },
+
+    esc() {
+      this.set('displayAccessPanel', false);
+      this.transitionToRoute(this.get('accessPanelConfig.backRoute'));
+    },
+
+    goToEntity(entity) {
+      this.set('searchQuery', '');
+      this.set('displayAccessPanel', false);
+      this.transitionToRoute(
+        this.get('accessPanelConfig.backRoute'),
+        entity.get('id')
+      );
+    }
+  }
+});
