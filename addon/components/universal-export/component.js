e826e51d6ebec2e0598d4bfb97bd574c95b58157
tgallice <tmgallice@gmail.com>
2018-02-12 16:13:35 +0100

    Add missing data

--- addon/components/universal-export/component.js
+++ addon/components/universal-export/component.js
@@ -123,7 +123,7 @@ export default Component.extend({
         to,
         this.get('selectedInfluencerIds'),
         this.get('filters'),
-      ).then(() => {
+      ).then((data) => {
         defer.resolve();
         this._onSuccessfullExport(to);
 
