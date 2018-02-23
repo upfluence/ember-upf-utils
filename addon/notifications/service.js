f35dd2fce3d82dc54c2006edc28870cee1ac9b73
Philippe Ndiaye <phndiaye@gmail.com>
2018-02-23 14:04:40 +0100

    linter love

--- addon/notifications/service.js
+++ addon/notifications/service.js
@@ -12,12 +12,11 @@ export default Service.extend({
   session: inject.service(),
 
   notificationReadURL: computed('session.data.authenticated.access_token', function() {
-    const url = Configuration.meURL;
-    const token = encodeURIComponent(
+    let token = encodeURIComponent(
       this.get('session.data.authenticated.access_token')
     );
 
-    return `${url}/notifications/mark_as_read?access_token=${token}`;
+    return `${Configuration.meURL}/notifications/mark_as_read?access_token=${token}`;
   }),
 
   markAsRead(model, notificationType) {
