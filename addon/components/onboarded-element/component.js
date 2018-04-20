40f0b7d665386963c6db3520f671e1493f81a91e
Philippe Ndiaye <phndiaye@gmail.com>
2018-04-20 11:58:22 +0200

    linter love

--- addon/components/onboarded-element/component.js
+++ addon/components/onboarded-element/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import OnbardingSteps from 'facade-web/resources/onboarding';
 
 const SHEPHERD_DEFAULT_CLASSES = 'shepherd upf-shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text';
-const { Component, computed, inject, run } = Ember;
+const { Component, computed, inject } = Ember;
 
 export default Component.extend({
   tour: inject.service(),
