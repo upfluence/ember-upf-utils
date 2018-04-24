bb3b0e2dcfc649a19d713a00ed2a59c56947eeac
Philippe Ndiaye <phndiaye@gmail.com>
2018-04-24 15:41:42 +0200

    rework the split overlay to always display the toggled side + use upf-icons

--- addon/components/split-overlay-area/component.js
+++ addon/components/split-overlay-area/component.js
@@ -11,25 +11,6 @@ export default Component.extend({
     'leftToggled:split-overlay-area--left-toggled'
   ],
 
-  mouseMove(e) {
-    let onLeftSide = (e.target.className.includes('left-side')
-      || e.target.className.includes('fa-close'));
-    let onRightSide = (e.target.className.includes('right-side')
-      || e.target.className.includes('fa-check'));
-    if (onLeftSide) {
-      this.$('.right-side').addClass('right-side--neutral');
-      this.$('.left-side').removeClass('left-side--neutral');
-    } else if (onRightSide) {
-      this.$('.left-side').addClass('left-side--neutral');
-      this.$('.right-side').removeClass('right-side--neutral');
-    }
-  },
-
-  mouseLeave() {
-    this.$('.right-side').removeClass('right-side--neutral');
-    this.$('.left-side').removeClass('left-side--neutral');
-  },
-
   actions: {
     leftSideClick() {
       this.sendAction('leftClick');
