f16cae5571fd944c111a80c81adae61e00a034f3
Toon van Ramshorst <ramshorst@gmail.com>
2018-05-22 10:47:20 +0200

    Activate tooltips

--- addon/components/influencer/profile-description/component.js
+++ addon/components/influencer/profile-description/component.js
@@ -1,34 +1,37 @@
 import Ember from 'ember';
 import layout from './template';
+import TooltipActivationMixin from 'ember-upf-utils/mixins/tooltip-activation';
 
 const { Component, computed } = Ember;
 
-export default Component.extend({
-  layout,
-  classNames: ['profile-description'],
+export default Component.extend(
+  TooltipActivationMixin, {
+    layout,
+    classNames: ['profile-description'],
 
-  isSelectable: true,
-  toggleAction: null,
-  selected: null,
-  selectedIcon: 'check',
-  overlayType: 'selection',
+    isSelectable: true,
+    toggleAction: null,
+    selected: null,
+    selectedIcon: 'check',
+    overlayType: 'selection',
 
-  presentInLists: computed.gt('profile.lists.length', 0),
+    presentInLists: computed.gt('profile.lists.length', 0),
 
-  // Avoid BC break with profile.selected
-  _selected: computed('selected', 'profile.selected', {
-    get() {
-      return this.get('selected') || this.get('profile.selected');
-    },
-    set(_, value) {
-      // selected not set
-      if (this.get('selected') === null) {
-        this.set('profile.selected', value);
-      } else {
-        this.set('selected', value);
+    // Avoid BC break with profile.selected
+    _selected: computed('selected', 'profile.selected', {
+      get() {
+        return this.get('selected') || this.get('profile.selected');
+      },
+      set(_, value) {
+        // selected not set
+        if (this.get('selected') === null) {
+          this.set('profile.selected', value);
+        } else {
+          this.set('selected', value);
+        }
+
+        return value;
       }
-
-      return value;
-    }
-  })
-});
+    })
+  }
+);
