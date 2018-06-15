e446d9f6c960a7e12a65a56ea6aa7ad084b928f3
Toon van Ramshorst <ramshorst@gmail.com>
2018-06-15 16:43:03 +0200

    Changing the way we feed Toast options to apps

--- config/environment.js
+++ config/environment.js
@@ -1,17 +1,19 @@
 /*jshint node:true*/
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return {
-    modulePrefix: 'ember-upf-utils',
+module.exports = function(environment, appConfig) {
+  appConfig['ember-toastr'] = {
+    toastrOptions: {
+      positionClass: 'toast-bottom-center upf-toastr--container',
+      hideDuration: 200,
+      timeOut: 12000,
+      newestOnTop: true,
+      closeButton: true,
+      extendedTimeOut: 6000
+    }
+  };
 
-    'ember-toastr': {
-      toastrOptions: {
-        positionClass: 'toast-bottom-center upf-toastr--container',
-        hideDuration: 200,
-        timeOut: 10000,
-        newestOnTop: true
-      }
-    },
+  return {
+    modulePrefix: 'ember-upf-utils'
   };
 };
