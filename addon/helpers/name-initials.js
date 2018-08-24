0a8d98539c1461ae1763b9140460f0f778092c63
Philippe Ndiaye <phndiaye@gmail.com>
2018-08-24 15:49:39 +0200

    linter lvoe

--- addon/helpers/name-initials.js
+++ addon/helpers/name-initials.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 
 const { Helper } = Ember;
 
-export function nameInitials(params/*, hash*/) {
+export function nameInitials(params) {
   let [name] =  params;
   let text = name.trim().toLowerCase();
   return text.replace(/\s[^a-z]/gi, '').split(' ').reduce((acc, word) => {
-    let trademarkSign = acc.length > 0 && acc[acc.length-1].startsWith('t') && word.startsWith('m');
+    let trademarkSign = acc.length > 0 && acc[acc.length - 1].startsWith('t')
+      && word.startsWith('m');
 
     if (!trademarkSign) {
       acc.push(word[0]);
@@ -14,7 +15,6 @@ export function nameInitials(params/*, hash*/) {
 
     return acc;
   }, []).slice(0, 3).join('');
-  return params;
 }
 
 export default Helper.helper(nameInitials);
