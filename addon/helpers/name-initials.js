1aa45cdb7fb61a1f928dfaa9586e2c276c00b549
Philippe Ndiaye <phndiaye@gmail.com>
2018-09-10 14:32:12 +0200

    avoid the trademark spotting overhead by joining with a hair space

--- addon/helpers/name-initials.js
+++ addon/helpers/name-initials.js
@@ -6,15 +6,9 @@ export function nameInitials(params) {
   let [name] =  params;
   let text = name.trim().toLowerCase();
   return text.replace(/\s[^a-z]/gi, '').split(' ').reduce((acc, word) => {
-    let trademarkSign = acc.length > 0 && acc[acc.length - 1].startsWith('t')
-      && word.startsWith('m');
-
-    if (!trademarkSign) {
-      acc.push(word[0]);
-    }
-
+    acc.push(word[0]);
     return acc;
-  }, []).slice(0, 3).join('');
+  }, []).slice(0, 3).join(String.fromCharCode(0x200a).toString());
 }
 
 export default Helper.helper(nameInitials);
