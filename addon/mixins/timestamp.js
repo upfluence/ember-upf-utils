4e8b21e4fe498c1665fabeca1279396154cb202e
Philippe Ndiaye <phndiaye@gmail.com>
2018-09-18 11:51:56 +0200

    add some utilities variables to the timestamp mixin

--- addon/mixins/timestamp.js
+++ addon/mixins/timestamp.js
@@ -1,15 +1,26 @@
 import Ember from 'ember';
+import moment from 'moment';
 
-export default Ember.Mixin.create({
-  fullDate: Ember.computed('timestamp', function() {
+const { Mixin, computed } = Ember;
+
+export default Mixin.create({
+  fullDate: computed('timestamp', function() {
     return new Date(this.get('timestamp') * 1000).toString();
   }),
 
-  dayMonth: Ember.computed('timestamp', function() {
+  dayMonth: computed('timestamp', function() {
     return new Date(
       this.get('timestamp') * 1000
     ).toLocaleDateString(
       'en-US', {day: '2-digit', month: 'short', year: 'numeric'}
     );
+  }),
+
+  formattedDate: computed('timestamp', function() {
+    return moment.unix(this.get('timestamp')).format('MMMM DD, YYYY');
+  }),
+
+  sinceDate: computed('timestamp', function() {
+    return moment(moment.unix(this.get('timestamp'))).fromNow();
   })
 });
