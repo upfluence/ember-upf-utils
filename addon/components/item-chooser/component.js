20d7713e2ba073b1786f2fa8be821430f38a57f9
Philippe Ndiaye <phndiaye@gmail.com>
2018-12-18 15:40:55 +0100

    depreciate ember-cli-selectize in favor of ember-power-select

--- addon/components/item-chooser/component.js
+++ addon/components/item-chooser/component.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 import layout from './template';
-import ExportEntity from 'ember-upf-utils/export-entity/model';
 
 const {
   Component,
-  inject,
   computed,
   isBlank
 } = Ember;
@@ -12,22 +10,22 @@ const {
 export default Component.extend({
   layout,
 
-  store: inject.service(),
   classNames: ['item-chooser'],
 
   // Options
   placeholder: 'Pick an item',
   recordType: null,
-  createOptionPlaceholder: '<strong>#item#</strong>',
+  createOptionPlaceholder: '#item#',
   multiple: false,
   canCreate: false,
   didCreate: '',
   optionValuePath: 'content',
-  optionLabelPath: 'content.name',
+  optionLabelPath: 'name',
   sortField: 'name',
   onBlur: null,
   recordTypeIsModel: false,
   disabled: false,
+  searchTerm: null,
 
   didReceiveAttrs() {
     if (this.get('canCreate') && this.get('recordTypeIsModel') && this.get('recordType') === null) {
@@ -35,41 +33,17 @@ export default Component.extend({
     }
   },
 
-  createFunctionName: computed('canCreate', function() {
-    return this.get('canCreate') ? 'create' : null;
+  createItemComponent: computed('canCreate', 'searchTerm', function() {
+    if (this.get('canCreate') && !isBlank(this.get('searchTerm'))) {
+      return 'item-chooser/create-item';
+    }
+
+    return null;
   }),
 
   actions: {
-    create(itemName) {
-      if (isBlank(itemName)) {
-        return;
-      }
-
-      let item = null;
-      if (this.get('recordTypeIsModel')) {
-        item = this.get('store').createRecord(this.get('recordType'), {
-          name: itemName
-        });
-      } else {
-        item = new ExportEntity({ name: itemName });
-      }
-
-      if (this.get('multiple')) {
-        this.get('selection').pushObject(item);
-      } else {
-        this.set('selection', item);
-        this.$('input').blur();
-      }
-
-      if (this.get('didCreate')) {
-        this.sendAction('didCreate', item);
-      }
-    },
-
-    optionCreate(value, escape) {
-      let compiled = this.get('createOptionPlaceholder')
-        .replace('#item#', escape(value.input));
-      return `<div class="create">${compiled}</div>`;
+    updateSearchTerm(term) {
+      this.set('searchTerm', term);
     }
   }
 });
