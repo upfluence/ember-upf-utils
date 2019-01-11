2f6d2b876967e39b1e78d658804ef89c10867ef7
Philippe Ndiaye <phndiaye@gmail.com>
2019-01-11 10:14:17 +0100

    a wrapper component for displaying or not a feature based on flag

---
+++ addon/components/feature-flagged/component.js
@@ -0,0 +1,18 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import layout from './template';
+
+
+export default Component.extend({
+  layout,
+
+  featureFlagsManager: service(),
+
+  didInsertElement() {
+    this.featureFlagsManager.allow(this.requestedFeature).then((allowedFeature) => {
+      this.set('allowedFeature', allowedFeature)
+    });
+  }
+}).reopenClass({
+  positionalParams: ['requestedFeature']
+});
---
+++ app/components/feature-flagged/component.js
@@ -0,0 +1,1 @@
+export { default } from '@upfluence/ember-upf-utils/components/feature-flagged/component';
