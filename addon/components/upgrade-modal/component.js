3847298f185199dc5b8b42e9caaa5a55b50dd68d
Philippe Ndiaye <phndiaye@gmail.com>
2019-03-26 11:09:01 +0100

    use assetMap to resolve upgrade feature image

--- addon/components/upgrade-modal/component.js
+++ addon/components/upgrade-modal/component.js
@@ -1,11 +1,14 @@
 import { getOwner } from '@ember/application';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 import layout from './template';
 
 
 export default Component.extend({
   layout,
+
+  assetMap: service(),
 
   hidden: true,
   to: null,
@@ -14,6 +17,12 @@ export default Component.extend({
     return getOwner(this).resolveRegistration(
       'config:environment'
     ).identityURL;
+  }),
+
+  upgradeFeatureImagePath: computed('to', function() {
+    return this.assetMap.resolve(
+      `assets/@upfluence/ember-upf-utils/images/${this.to}.png`
+    );
   }),
 
   actions: {
