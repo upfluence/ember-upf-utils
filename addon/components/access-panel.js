31a10c4e49c6b141a5e6b399a9676895fb1c3d7a
Philippe Ndiaye <phndiaye@gmail.com>
2019-04-05 15:18:11 +0200

    update to match the new oss's upf-table actions hooks

--- addon/components/access-panel.js
+++ addon/components/access-panel.js
@@ -4,13 +4,15 @@ export default Component.extend({
   hasSelection: true,
   hasSearch: true,
   hasPolymorphicColumns: true,
-  onRowClickCallback: 'goToEntity',
   contentLoading: false,
+
+  onRowClick() {},
+  performSearch() {},
 
   hasPagination: null,
   currentPage: 1,
   totalPages: 1,
   perPage: 1,
   itemTotal: 0,
-  itemCount: 0
+  itemCount: 0,
 });
