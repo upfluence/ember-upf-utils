b0aac35d126ba1431ece5aa4a1fa27c014814eb8
Philippe Ndiaye <phndiaye@gmail.com>
2019-05-16 11:16:54 +0200

    remove unused router service

--- addon/components/smart-link-to/component.js
+++ addon/components/smart-link-to/component.js
@@ -1,13 +1,10 @@
 import { getOwner } from '@ember/application';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
 import layout from './template';
 
 export default Component.extend({
   layout,
-
-  router: service(),
 
   cssClass: '',
   target: '',
