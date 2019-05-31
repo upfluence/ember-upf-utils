ac2960b15700456e6811912108473c47571e94e4
Min S <mindysoh@gmail.com>
2019-05-31 12:24:27 -0400

    basic window with power-select

--- addon/components/universal-import/component.js
+++ addon/components/universal-import/component.js
@@ -1,10 +1,23 @@
 import Component from '@ember/component';
 import layout from './template';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   layout,
 
   exports: service(),
   toast: service(),
-  
+
+  _canCreate: true,
+  items: null,
+  createOptionPlaceholder: 'Create list',
+  placeholder: 'Pick or create a list',
+
+  actions: {
+    didCreateItem(item) {
+      let _i = this.get('items');
+      _i.pushObject(item);
+      this.set('items', _i);
+    }    
+  }
 });
