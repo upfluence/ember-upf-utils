3572ddad97760296fd4e3d179145950fedd33d6f
Min S <mindysoh@gmail.com>
2019-05-31 16:36:14 -0400

    render all entities that match keyword on the universal import component

--- addon/components/universal-import/component.js
+++ addon/components/universal-import/component.js
@@ -1,13 +1,11 @@
 import Component from '@ember/component';
 import layout from './template';
 import { inject as service } from '@ember/service';
-import ExportEntity from '@upfluence/ember-upf-utils/export-entity/model';
 
 export default Component.extend({
   layout,
 
   imports: service(),
-  exports: service(),
   toast: service(),
 
   _model: '',
@@ -15,19 +13,6 @@ export default Component.extend({
   items: null,
   createOptionPlaceholder: 'Create list',
   placeholder: 'Pick or create a list',
-
-  didInsertElement() {
-  // need to fetch from all, not just list.
-    // this.get('exports').fetchEntities('list', (response) => {
-    //   this.set('items', response.entities.map((item) => {
-    //     return ExportEntity.create(item);
-    //   }));
-    // });
-
-    this.get('imports').fetchKeywordResults('mailing', (response) => {
-      console.log(response);
-    });
-  },
 
   actions: {
     didCreateItem(item) {
@@ -39,7 +24,13 @@ export default Component.extend({
     searchEntities(keyword) {
     // call import service here
       this.get('imports').fetchKeywordResults(keyword, (response) => {
-        console.log(response);
+        let allItems = [];
+
+        Object.keys(response).forEach(key => {
+          response[key].map((item) => allItems.push(item));
+        });
+
+        this.set('items', allItems);     
       });  
     },
   }
