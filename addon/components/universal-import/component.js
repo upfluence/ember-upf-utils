f4ce05cf91cafdf26f7a56169426056d22b19c17
Min S <mindysoh@gmail.com>
2019-06-04 15:51:20 -0400

    cleanup, shifted styles to app/styles

--- addon/components/universal-import/component.js
+++ addon/components/universal-import/component.js
@@ -6,34 +6,22 @@ export default Component.extend({
   layout,
 
   imports: service(),
-  toast: service(),
 
   _model: '',
   _canCreate: false,
   items: null,
-  createOptionPlaceholder: 'Create list',
-  placeholder: 'Pick or create a list',
+  placeholder: 'Pick a list',
 
   actions: {
-    didCreateItem(item) {
-      let _i = this.get('items');
-      _i.pushObject(item);
-      this.set('items', _i);
-    },
-
     searchEntities(keyword) {
-    // call import service here
       this.get('imports').fetchKeywordResults(keyword, (response) => {
-        console.log(response);
         let allItems = [];
 
         Object.keys(response).forEach(key => {
           response[key].map((item) => allItems.push({icon: key, item}));
-          // response[key].map((item) => allItems.push(item));
         });
 
         this.set('items', allItems);
-        console.log(allItems);  
       });  
     },
   }
