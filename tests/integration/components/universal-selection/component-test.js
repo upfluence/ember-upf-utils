1d73c00ee577b385a79e0c7416ed54966ed97df9
Min S <mindysoh@gmail.com>
2019-06-05 13:41:33 -0400

    rename universal import to universal selection

--- tests/integration/components/universal-selection/component-test.js
+++ tests/integration/components/universal-selection/component-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | universal-import', function(hooks) {
+module('Integration | Component | universal-selection', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{universal-import}}`);
+    await render(hbs`{{universal-selection}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#universal-import}}
+      {{#universal-selection}}
         template block text
-      {{/universal-import}}
+      {{/universal-selection}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
