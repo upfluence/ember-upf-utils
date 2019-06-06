a9c821964ce400d405fe7760dbdc6b93a04e8c58
Min S <mindysoh@gmail.com>
2019-06-06 16:45:32 -0400

    removed redundant code

--- addon/components/universal-selection/component.js
+++ addon/components/universal-selection/component.js
@@ -1,8 +1,6 @@
 import Component from '@ember/component';
 import EmberObject from '@ember/object';
 import { inject as service } from '@ember/service';
-import { observer } from '@ember/object';
-import { alias } from '@ember/object/computed';
 
 import layout from './template';
 
@@ -14,7 +12,6 @@ export default Component.extend({
   _model: '',
   items: null,
   placeholder: 'Select a list to import influencers',
-  selectedEntity: alias('current'),
   current: null,
 
   actions: {
