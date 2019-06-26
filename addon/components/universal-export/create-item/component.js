2089341a04c9e5cb7d56914d51ac471da4af00bd
Philippe Ndiaye <phndiaye@gmail.com>
2019-06-26 17:43:31 +0200

    base the createable entities off the user's scopes

--- addon/components/universal-export/create-item/component.js
+++ addon/components/universal-export/create-item/component.js
@@ -1,15 +1,43 @@
 import Component from '@ember/component';
-import layout from './template';
+import { empty } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
-const CREATABLE_ENTITIES = ['list', 'mailing', 'stream'];
+import layout from './template';
 
 export default Component.extend({
   layout,
 
+  currentUser: service(),
+
   classNames: ['padding-xx-sm'],
 
-  createableEntities: CREATABLE_ENTITIES,
-  selectedEntityType: 'list',
+  createableEntities: [],
+  selectedEntityType: null,
+
+  noCreateableEntities: empty('createableEntities'),
+
+  init() {
+    this._super();
+    this.currentUser.fetch().then(({ user }) => {
+      let _ce = [];
+      if (user.granted_scopes.includes('facade_web')) {
+        _ce.push('list');
+      }
+
+      if (user.granted_scopes.includes('inbox_client')) {
+        _ce.push('mailing');
+      }
+
+      if (user.granted_scopes.includes('analytics_web')) {
+        _ce.push('stream');
+      }
+
+      this.set('createableEntities', _ce);
+      this.set('selectedEntityType', _ce[0]);
+    });
+
+    console.log(this.createableEntities);
+  },
 
   didRender() {
     this.$('.dropdown-toggle').dropdown();
