77d5923be0e8eb95cb7369639392290611490fe4
Min S <mindysoh@gmail.com>
2019-07-16 14:46:47 -0400

    remove the icon for options

--- addon/components/universal-selection/option/component.js
+++ addon/components/universal-selection/option/component.js
@@ -1,22 +1,9 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-
 import layout from './template';
-
-const ENTITIES_ICONS = {
-  stream: 'upf-icon--monitor',
-  list: 'upf-icon--search',
-  mailing: 'upf-icon--inbox',
-  campaign: 'upf-icon--workflow'
-};
 
 export default Component.extend({
   layout,
 
   tagName: 'li',
-  classNames: ['universal-selection-option'],
-
-  iconName: computed('item', function() {
-    return ENTITIES_ICONS[this.item.type];
-  })
+  classNames: ['universal-selection-option']
 });
