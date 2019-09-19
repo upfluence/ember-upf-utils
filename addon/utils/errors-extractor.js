796109081081696b837d84d057180ee17c1d39c1
Min S <mindysoh@gmail.com>
2019-09-19 10:20:07 -0400

    rename i18n to intl

--- addon/utils/errors-extractor.js
+++ addon/utils/errors-extractor.js
@@ -1,11 +1,11 @@
-export default function (payload, i18n) {
+export default function (payload, intl) {
   let errors = [];
 
   (payload.errors || []).forEach((e) => {
     if (typeof e === 'string') {
       errors.push(e);
     } else if (e.code && e.field && e.resource) {
-      errors.push(i18n.t(
+      errors.push(intl.t(
         `${e.resource}.${e.field}.${e.code}`
       ));
     }
