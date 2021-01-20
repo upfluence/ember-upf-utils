1e5419e15fe73b17901800e4670c47e8aaea8d81
Philippe Ndiaye <phndiaye@gmail.com>
2021-01-20 16:53:28 +0100

    country-chooser : reimplemented in Glimmer Components

---
+++ addon/components/country-chooser.js
@@ -0,0 +1,23 @@
+import Component from '@glimmer/component';
+import CountryCodes from '@upfluence/ember-upf-utils/resources/country-codes';
+
+export default class CountryChooserComponent extends Component {
+  codes = CountryCodes
+
+  // Named Arguments Defaults.
+  // -------------------------
+  get label() { return this.args.label || 'Country'; }
+  get placeholder() { return this.args.placeholder || '-'; }
+  get required() { return this.args.required || false; }
+  get multiple() { return this.args.multiple || true; }
+  get dark() { return this.args.dark || false; }
+  get size() { return this.args.size || null; }
+
+  get countryCode() {
+    return (this._selection || {}).id;
+  }
+
+  get countryCodes() {
+    return (this._selection || []).map((x) => x.id);
+  }
+}
