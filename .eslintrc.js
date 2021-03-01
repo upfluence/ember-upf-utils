bb9fb528c2c3acb4171467024ca13225ee8cd95f
Philippe Ndiaye <phndiaye@gmail.com>
2021-03-01 11:55:40 +0100

    additional linter rules settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,13 @@ module.exports = {
     'ember/no-mixins': 'off',
     'ember/no-get': 'off',
     'ember/avoid-leaking-state-in-ember-objects': 'off',
-    'ember/closure-actions': 'off'
+    'ember/closure-actions': 'off',
+    'ember/no-global-jquery': 'off',
+    'ember/no-classic-classes': 'off',
+    'ember/no-classic-components': 'off',
+    'ember/require-tagless-components': 'off',
+    'ember/no-actions-hash': 'off',
+    'ember/no-component-lifecycle-hooks': 'off'
   },
   overrides: [
     // node files
