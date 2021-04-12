a99c5be9175fe5c2f414087954a1f1c15d5c1a64
Julien Vannier <julien.vannier66@gmail.com>
2021-04-12 18:58:27 +0200

    Fix linter

--- addon/components/http-errors-code/component.js
+++ addon/components/http-errors-code/component.js
@@ -1,11 +1,9 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
 import layout from './template';
 
 export default Component.extend({
   layout,
-  
+
   actions: {
     openIntercom() {
       if (window.Intercom) {
