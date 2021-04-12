7d36b5d3aa87a7071f4b01a35e55756fb2aeb6c8
Julien Vannier <julien.vannier66@gmail.com>
2021-04-12 10:53:26 +0200

    Move identityUrl to const

--- addon/decorators/deactivated-account-handler.js
+++ addon/decorators/deactivated-account-handler.js
@@ -1,13 +1,13 @@
+//TODO: Change that with variable based on environnement
+const IDENTITY_URL = 'https://user.upfluence.co/';
+
 export default function deactivatedAccountHandler(Class) {
   return class DeactivatedAccountHandler extends Class {
     setupController(controller, error) {
       super.setupController(controller, error);
 
-      //TODO: Change that with variable based on environnement
-      const identityURL = 'https://user.upfluence.co/';
-
       if (error.errors && error.errors[0] && error.errors[0].code === 'on_hold') {
-        window.location = `${identityURL}accounts/billing?status=on_hold`;
+        window.location = `${IDENTITY_URL}accounts/billing?status=on_hold`;
       }
     }
   };
