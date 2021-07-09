bf2a031e79b38359a2538c56b751564e095e503a
Philippe Ndiaye <phndiaye@gmail.com>
2021-07-09 14:31:49 +0200

    build pdf embed node

--- addon/components/u-edit/shared-triggers/pdf.js
+++ addon/components/u-edit/shared-triggers/pdf.js
@@ -12,7 +12,26 @@ export default class UEditSharedTriggersPdfComponent extends Component {
   }
 
   _buildNode(url) {
+    let container = document.createElement('div');
+    let spacing = document.createElement('br');
 
+    let embed = document.createElement('embed');
+    embed.setAttribute('src', url);
+    embed.setAttribute('width', '100%');
+    embed.setAttribute('height', '800px');
+    embed.setAttribute('type', 'application/pdf');
+
+    let embedNotSupported = document.createElement('a');
+    embedNotSupported.setAttribute('href', url);
+    embedNotSupported.textContent = 'Click to open PDF';
+
+    container.append(spacing);
+    container.appendChild(embed);
+    container.append(spacing);
+    container.appendChild(embedNotSupported);
+    container.append(spacing);
+
+    return container;
   }
 
   @action
