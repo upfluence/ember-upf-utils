72744f8e0ec89153da6bb24db82d9262f23a04f8
tgallice <tmgallice@gmail.com>
2021-10-13 14:33:12 +0200

    utils/param: Move param serializer

--- addon/helpers/sign-url.js
+++ addon/helpers/sign-url.js
@@ -1,6 +1,7 @@
 import { inject as service } from '@ember/service';
 import Helper from '@ember/component/helper';
 import { observer } from '@ember/object';
+import serializeParams from '@upfluence/ember-upf-utils/utils/serialize-params';
 
 export default Helper.extend({
   session: service(),
@@ -10,7 +11,7 @@ export default Helper.extend({
 
     extra.access_token = this.get('session.data.authenticated.access_token');
 
-    return `${url}?${new URLSearchParams(Object.entries(extra)).toString()}`;
+    return `${url}?${serializeParams(extra)}`;
   },
 
   _: observer('session.data.authenticated.access_token', function () {
