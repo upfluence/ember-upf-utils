1574cb91b6b649d7bbadee00e8edc8c5a61905cd
MaximilienBrunelle <maximilien.brunelle@gmail.com>
2022-07-11 16:23:46 +0200

    Migrated: publishr-admin EnableUTMs to UtmLinkBuilder

---
+++ addon/components/utm-link-builder.ts
@@ -0,0 +1,72 @@
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+interface UtmLinkBuilderArgs {
+  url: string;
+  onChange(url: string, utmsEnabled: boolean, formValid: boolean, utmFields: UTM_FIELDS): void;
+}
+
+type UTM_FIELDS = {
+  utm_source: string;
+  utm_medium: string;
+  utm_campaign: string;
+};
+
+export default class UtmLinkBuilder extends Component<UtmLinkBuilderArgs> {
+  @tracked utmsEnabled: boolean = false;
+  @tracked utmSource: string = '';
+  @tracked utmMedium: string = '';
+  @tracked utmCampaign: string = '';
+
+  get utmsValid(): boolean {
+    return !!this.utmSource && !!this.utmMedium && !!this.utmCampaign;
+  }
+
+  @action
+  toggleSwitchUpdate(newValue: boolean): void {
+    this.utmsEnabled = newValue;
+    if (!newValue) {
+      this.utmCampaign = '';
+      this.utmMedium = '';
+      this.utmSource = '';
+    }
+    this.notifyChanges();
+  }
+
+  @action
+  notifyChanges(target?: string): void {
+    // @ts-ignore
+    if (target) this[target] = this[target].replaceAll(' ', '+');
+    console.log(this.previewUrl, this.utmsEnabled, this.utmsValid);
+    this.args.onChange(this.previewUrl, this.utmsEnabled, this.utmsValid, this.utmFields);
+  }
+
+  get utmFields(): UTM_FIELDS {
+    return {
+      utm_campaign: this.utmCampaign,
+      utm_medium: this.utmMedium,
+      utm_source: this.utmSource
+    };
+  }
+
+  get previewUrl(): string {
+    return `${this.fieldUrl}?utm_source=${this.fieldSource}&utm_medium=${this.fieldMedium}&utm_campaign=${this.fieldCampaign}`;
+  }
+
+  get fieldUrl(): string {
+    return this.args.url || '{link_url}';
+  }
+
+  get fieldSource(): string {
+    return this.utmSource || '{source_field}';
+  }
+
+  get fieldMedium(): string {
+    return this.utmMedium || '{medium_field}';
+  }
+
+  get fieldCampaign(): string {
+    return this.utmCampaign || '{campaign_field}';
+  }
+}
---
+++ tests/integration/components/utm-link-builder-test.ts
@@ -0,0 +1,87 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+// import engineResolverFor from 'ember-engines/test-support/engine-resolver-for';
+import hbs from 'htmlbars-inline-precompile';
+import click from '@ember/test-helpers/dom/click';
+import sinon from 'sinon';
+import typeIn from '@ember/test-helpers/dom/type-in';
+import settled from '@ember/test-helpers/settled';
+
+module('Integration | Component | utm-link-builder', function (hooks) {
+  setupRenderingTest(hooks);
+  hooks.beforeEach(function () {
+    this.onChange = sinon.spy();
+  });
+
+  test('it renders', async function (assert) {
+    await render(hbs`<UtmLinkBuilder />`);
+
+    assert.dom('.utm-container').exists();
+  });
+
+  test('If the toggle is disabled, the UTM inputs are hidden', async function (assert) {
+    await render(hbs`<UtmLinkBuilder />`);
+
+    assert.dom('[data-control-name="utm_source_input"]').doesNotExist();
+    assert.dom('[data-control-name="utm_medium_input"]').doesNotExist();
+    assert.dom('[data-control-name="utm_campaign_input"]').doesNotExist();
+  });
+
+  test('If the toggle is enabled, the UTM inputs are visible', async function (assert) {
+    await render(hbs`<UtmLinkBuilder @onChange={{this.onChange}} />`);
+    await click('.upf-toggle');
+    assert.dom('[data-control-name="utm_source_input"]').exists();
+    assert.dom('[data-control-name="utm_medium_input"]').exists();
+    assert.dom('[data-control-name="utm_campaign_input"]').exists();
+  });
+
+  test('The @onChange method is called when inputs are updated', async function (assert) {
+    let utmFields = { utm_source: '', utm_campaign: '', utm_medium: '' };
+    await render(hbs`<UtmLinkBuilder @onChange={{this.onChange}} />`);
+    await click('.upf-toggle');
+    assert.true(
+      this.onChange.calledWith(
+        '{link_url}?utm_source={source_field}&utm_medium={medium_field}&utm_campaign={campaign_field}',
+        true,
+        false,
+        utmFields
+      )
+    );
+
+    utmFields.utm_source = 'a';
+    await typeIn('[data-control-name="utm_source_input"] .upf-input', 'a');
+    assert.true(
+      this.onChange.calledWith(
+        '{link_url}?utm_source=a&utm_medium={medium_field}&utm_campaign={campaign_field}',
+        true,
+        false,
+        utmFields
+      )
+    );
+
+    utmFields.utm_medium = 'b';
+    await typeIn('[data-control-name="utm_medium_input"] .upf-input', 'b');
+    assert.true(
+      this.onChange.calledWith(
+        '{link_url}?utm_source=a&utm_medium=b&utm_campaign={campaign_field}',
+        true,
+        false,
+        utmFields
+      )
+    );
+
+    utmFields.utm_campaign = 'c';
+    await typeIn('[data-control-name="utm_campaign_input"] .upf-input', 'c');
+    assert.true(this.onChange.calledWith('{link_url}?utm_source=a&utm_medium=b&utm_campaign=c', true, true, utmFields));
+    await settled();
+  });
+
+  test('If a space character is inputed, it is replaced with a + sign', async function (assert) {
+    await render(hbs`<UtmLinkBuilder @onChange={{this.onChange}} />`);
+    await click('.upf-toggle');
+    await typeIn('[data-control-name="utm_source_input"] .upf-input', 'a a');
+    await settled();
+    assert.dom('[data-control-name="utm_source_input"] .upf-input').hasValue('a+a');
+  });
+});
