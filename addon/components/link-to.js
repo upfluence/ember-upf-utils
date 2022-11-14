a55f6242ff82f2fdb3c594feac870809045cb052
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2022-11-14 10:50:30 +0100

    Prep for Ember 3.28 migration (#351)
    * remove overwritten link-to component
    
    * extend table-fluid/row-link from the engine's LinkTo
    
    * fix cp-validation version
    
    * remove table-fluid related code now that facade-web is only project that needs it

--- addon/components/link-to.js
+++
@@ -1,4 +0,0 @@
-import LinkComponent from '@ember/routing/link-component';
-import TooltipActivationMixin from '@upfluence/ember-upf-utils/mixins/tooltip-activation';
-
-export default LinkComponent.reopen(TooltipActivationMixin);
