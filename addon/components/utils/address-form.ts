4ecd0618fa4937e9ecad57fc0f19a0dfe4c82359
Julien Vannier <julien.vannier66@gmail.com>
2023-11-01 15:37:15 +0100

    PR feedback

--- addon/components/utils/address-form.ts
+++ addon/components/utils/address-form.ts
@@ -3,10 +3,10 @@ import { tracked } from '@glimmer/tracking';
 import { action, get, set } from '@ember/object';
 import { isEmpty } from '@ember/utils';
 import { isTesting } from '@embroider/macros';
+import { getOwner } from '@ember/application';
 
 import { Loader } from '@googlemaps/js-api-loader';
 import { CountryData, countries } from '@upfluence/oss-components/utils/country-codes';
-import { getOwner } from '@ember/application';
 
 interface UtilsAddressFormArgs {
   address: any;
@@ -34,38 +34,23 @@ export default class extends Component<UtilsAddressFormArgs> {
   }
 
   @action
-  onSetup() {
+  initAutoCompletion(): void {
     if (isTesting()) return;
     const loader = new Loader({
       apiKey: getOwner(this).resolveRegistration('config:environment').google_map_api_key,
       version: 'weekly'
     });
 
-    loader
-      .importLibrary('places')
-      .then(({ Autocomplete }) => {
-        const input = document.querySelector('[data-control-name="address-form-address1"] input') as HTMLInputElement;
-        const options = {
-          fields: ['address_components'],
-          strictBounds: false,
-          types: ['address']
-        };
-
-        const autocomplete = new Autocomplete(input, options);
-
-        autocomplete.addListener('place_changed', () => {
-          const place = autocomplete.getPlace();
-          this.fillInAddress(place);
-        });
-        input.addEventListener('focusout', (event) => {
-          if ((<HTMLInputElement>event.target).value !== this.args.address.address1) {
-            (<HTMLInputElement>event.target).value = this.args.address.address1;
-          }
-        });
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+    loader.importLibrary('places').then(({ Autocomplete }) => {
+      const input = document.querySelector('[data-control-name="address-form-address1"] input') as HTMLInputElement;
+      const options = {
+        fields: ['address_components'],
+        strictBounds: false,
+        types: ['address']
+      };
+      const autocomplete = new Autocomplete(input, options);
+      this.initInputListeners(autocomplete, input);
+    });
   }
 
   @action
@@ -74,40 +59,40 @@ export default class extends Component<UtilsAddressFormArgs> {
   }
 
   @action
-  applyCountry(country: Partial<CountryData>): void {
+  applyCountry(country: CountryData): void {
     if (get(this.args.address, 'countryCode') !== country.alpha2) {
       set(this.args.address, 'state', '');
     }
 
     set(this.args.address, 'countryCode', country.alpha2);
     this.provincesForCountry = country.provinces ?? null;
-    this.args.onChange(this.args.address, this._checkAddressValidity());
+    this.args.onChange(this.args.address, this.checkAddressValidity());
   }
 
   @action
   applyProvince(province?: ProvinceData): void {
     set(this.args.address, 'state', province?.name || '');
-    this.args.onChange(this.args.address, this._checkAddressValidity());
+    this.args.onChange(this.args.address, this.checkAddressValidity());
   }
 
   @action
   onFieldUpdate(): void {
-    this.args.onChange(this.args.address, this._checkAddressValidity());
+    this.args.onChange(this.args.address, this.checkAddressValidity());
   }
 
   @action
   onPhoneNumberUpdate(prefix: string, number: number): void {
     set(this.args.address, 'phone', prefix + number);
-    this.args.onChange(this.args.address, this._checkAddressValidity());
+    this.args.onChange(this.args.address, this.checkAddressValidity());
   }
 
   @action
   onPhoneNumberValidation(passes: boolean): void {
     this.validPhoneNumber = !isEmpty(this.phoneNumber) && passes;
-    this.args.onChange(this.args.address, this._checkAddressValidity());
+    this.args.onChange(this.args.address, this.checkAddressValidity());
   }
 
-  private _checkAddressValidity(): boolean {
+  private checkAddressValidity(): boolean {
     const validatedAddressFields = this.args.hideNameAttrs
       ? BASE_VALIDATED_ADDRESS_FIELDS
       : [...BASE_VALIDATED_ADDRESS_FIELDS, ...EXTRA_VALIDATED_ADDRESS_FIELDS];
@@ -126,45 +111,57 @@ export default class extends Component<UtilsAddressFormArgs> {
     });
   }
 
-  private fillInAddress(place: any): void {
+  private initInputListeners(autocomplete: google.maps.places.Autocomplete, input: HTMLElement): void {
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      this.fillInAddress(place);
+    });
+    input.addEventListener('focusout', (event) => {
+      if ((<HTMLInputElement>event.target).value !== this.args.address.address1) {
+        (<HTMLInputElement>event.target).value = this.args.address.address1;
+      }
+    });
+  }
+
+  private fillInAddress(place: google.maps.places.PlaceResult): void {
     let address1: string = '';
     let zipcode: string = '';
     let city: string = '';
 
-    for (const component of place.address_components.reverse()) {
-      const componentType = component.types[0];
-
-      switch (componentType) {
-        case 'street_number': {
+    place.address_components!.reverse().map((component) => {
+      const componentType: string = component.types[0];
+      const mapper: { [key: string]: () => void } = {
+        street_number: () => {
           address1 = `${component.long_name} ${address1}`;
-          break;
-        }
-        case 'route': {
+        },
+        route: () => {
           address1 += component.long_name;
-          break;
-        }
-        case 'postal_code': {
+        },
+        postal_code: () => {
           zipcode = `${component.long_name}${zipcode}`;
-          break;
-        }
-        case 'postal_code_suffix': {
+        },
+        postal_code_suffix: () => {
           zipcode = `${zipcode}-${component.long_name}`;
-          break;
-        }
-        case 'locality':
-        case 'postal_town':
+        },
+        locality: () => {
           city = component.long_name;
-          break;
-        case 'administrative_area_level_1': {
+        },
+        postal_town: () => {
+          city = component.long_name;
+        },
+        administrative_area_level_1: () => {
           set(this.args.address, 'state', component.long_name || '');
-          break;
-        }
-        case 'country':
+        },
+        country: () => {
           const selectedCountry = this.countries.find((c) => c.alpha2 === component.short_name);
-          this.applyCountry({ alpha2: component.short_name, provinces: selectedCountry!.provinces });
-          break;
-      }
-    }
+
+          if (!selectedCountry) return;
+          this.applyCountry(selectedCountry);
+        }
+      };
+      mapper[componentType]?.();
+    });
+
     set(this.args.address, 'address1', address1);
     set(this.args.address, 'address2', '');
     set(this.args.address, 'zipcode', zipcode);
