12c78d33158cf3c9200ed86ae52d004defc1fba3
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2024-02-13 17:04:45 +0100

    Rework 404/500 pages (#385)
    * Redesign 404 & 500 error state component
    
    * Setup ember-brand-manager w/ brand-based assets
    
    * Add tests
    
    * fix pr feedbacks
    
    * fix early qa feedbacks
    
    * add intl service typing
    
    * fix component height
    
    * group tests
    
    * update translations

---
+++ addon/components/http-errors-code.ts
@@ -0,0 +1,47 @@
+import Component from '@glimmer/component';
+import { getOwner } from '@ember/application';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+import type { IntlService } from 'ember-intl';
+
+interface HTTPErrorsCodeArgs {
+  httpError: '404' | '500';
+}
+
+export default class extends Component<HTTPErrorsCodeArgs> {
+  @service declare intl: IntlService;
+
+  csChat?: any;
+
+  self = location.href;
+
+  constructor(owner: unknown, args: HTTPErrorsCodeArgs) {
+    super(owner, args);
+
+    // We have been dealing w/ a bad design here which is that CSChat is defined in ember-identity and not in
+    // ember-upf-utils. It actually works because we are 100% sure that it will be present in the context of one of our
+    // running apps.
+    this.csChat = getOwner(this).lookup('service:cs-chat');
+  }
+
+  get errorHints(): { icon: string; label: string }[] {
+    if (this.args.httpError === '404') {
+      return [
+        { icon: 'fa-unlink', label: this.intl.t('errors.404.hints.url_accuracy_check') },
+        { icon: 'fa-key', label: this.intl.t('errors.404.hints.authorization_check') }
+      ];
+    }
+
+    return [
+      { icon: 'fa-bug', label: this.intl.t('errors.500.hints.technical_issue') },
+      { icon: 'fa-life-ring', label: this.intl.t('errors.500.hints.contact_support') }
+    ];
+  }
+
+  @action
+  openSupportChannel(event: PointerEvent): void {
+    event.stopPropagation();
+    this.csChat?.openTicket(this.intl.t(`errors.${this.args.httpError}.support_message`));
+  }
+}
---
+++ tests/integration/components/http-errors-code-test.ts
@@ -0,0 +1,111 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import Service from '@ember/service';
+// @ts-ignore
+import { setupIntl } from 'ember-intl/test-support';
+import sinon from 'sinon';
+
+class CSChat extends Service {
+  openTicket = sinon.stub();
+}
+
+module('Integration | Component | http-errors-code', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
+
+  hooks.beforeEach(function () {
+    this.owner.register('service:cs-chat', CSChat);
+    this.csChat = this.owner.lookup('service:cs-chat');
+  });
+
+  ['404', '500'].forEach((statusCode) => {
+    test('it renders w/ the right title & subtitle', async function (assert) {
+      this.httpError = statusCode;
+
+      await render(hbs`<HttpErrorsCode @httpError={{this.httpError}} />`);
+
+      assert.dom('[data-control-name="http-error-code-title"]').hasText(this.intl.t(`errors.${statusCode}.title`));
+      assert
+        .dom('[data-control-name="http-error-code-subtitle"]')
+        .hasText(this.intl.t(`errors.${statusCode}.subtitle`));
+    });
+  });
+
+  module('404 error', function () {
+    test('the right hints are displayed', async function (assert) {
+      await render(hbs`<HttpErrorsCode @httpError="404" />`);
+      assert.dom('[data-control-name="http-error-code-hints"] > div').exists({ count: 2 });
+      assert.dom('[data-control-name="http-error-code-hints"] > div:first-child i.far').hasClass('fa-unlink');
+      assert
+        .dom('[data-control-name="http-error-code-hints"] > div:first-child')
+        .hasText(this.intl.t('errors.404.hints.url_accuracy_check'));
+      assert.dom('[data-control-name="http-error-code-hints"] > div:last-child i.far').hasClass('fa-key');
+      assert
+        .dom('[data-control-name="http-error-code-hints"] > div:last-child')
+        .hasText(this.intl.t('errors.404.hints.authorization_check'));
+    });
+
+    test('the right actions are displayed', async function (assert) {
+      const redirectStub = sinon.stub(window, 'open');
+
+      await render(hbs`<HttpErrorsCode @httpError="404" />`);
+      assert.dom('.upf-btn').exists({ count: 2 });
+      assert.dom('.upf-btn.upf-btn--default').exists();
+      assert.dom('.upf-btn.upf-btn--default i.far').hasClass('fa-comment');
+      assert.dom('.upf-btn.upf-btn--default').hasText(this.intl.t('errors.404.contact_support'));
+      assert.dom('.upf-btn.upf-btn--primary').exists();
+      assert.dom('.upf-btn.upf-btn--primary i.far').hasClass('fa-arrow-left');
+      assert.dom('.upf-btn.upf-btn--primary').hasText(this.intl.t('errors.404.cta'));
+
+      await click('.upf-btn.upf-btn--default');
+      assert.ok(this.csChat.openTicket.calledOnceWithExactly(this.intl.t('errors.404.support_message')));
+      await click('.upf-btn.upf-btn--primary');
+      assert.ok(redirectStub.calledOnceWithExactly('/', '_self'));
+      redirectStub.restore();
+    });
+  });
+
+  module('500 error', function () {
+    test('the right hints are displayed', async function (assert) {
+      await render(hbs`<HttpErrorsCode @httpError="500" />`);
+
+      assert.dom('[data-control-name="http-error-code-hints"] > div').exists({ count: 2 });
+      assert.dom('[data-control-name="http-error-code-hints"] > div:first-child i.far').hasClass('fa-bug');
+      assert
+        .dom('[data-control-name="http-error-code-hints"] > div:first-child')
+        .hasText(this.intl.t('errors.500.hints.technical_issue'));
+      assert.dom('[data-control-name="http-error-code-hints"] > div:last-child i.far').hasClass('fa-life-ring');
+      assert
+        .dom('[data-control-name="http-error-code-hints"] > div:last-child')
+        .hasText(this.intl.t('errors.500.hints.contact_support'));
+    });
+
+    test('the right actions are displayed', async function (assert) {
+      const redirectStub = sinon.stub(window, 'open');
+
+      await render(hbs`<HttpErrorsCode @httpError="500" />`);
+      assert.dom('.upf-btn').exists({ count: 2 });
+      assert.dom('.upf-btn.upf-btn--default').exists();
+      assert.dom('.upf-btn.upf-btn--default i.far').hasClass('fa-comment');
+      assert.dom('.upf-btn.upf-btn--default').hasText(this.intl.t('errors.500.contact_support'));
+      assert.dom('.upf-btn.upf-btn--primary').exists();
+      assert.dom('.upf-btn.upf-btn--primary i.far').hasClass('fa-redo');
+      assert.dom('.upf-btn.upf-btn--primary').hasText(this.intl.t('errors.500.cta'));
+
+      await click('.upf-btn.upf-btn--default');
+      assert.ok(this.csChat.openTicket.calledOnceWithExactly(this.intl.t('errors.500.support_message')));
+      await click('.upf-btn.upf-btn--primary');
+      assert.ok(redirectStub.calledOnceWithExactly(location.href, '_self'));
+      redirectStub.restore();
+    });
+  });
+
+  test('When cs-chat button is missing, there is no Contact support button', async function (assert) {
+    this.owner.unregister('service:cs-chat');
+    await render(hbs`<HttpErrorsCode @httpError="500" />`);
+    assert.dom('.upf-btn').exists({ count: 1 });
+    assert.dom('.upf-btn.upf-btn--default').doesNotExist();
+  });
+});
