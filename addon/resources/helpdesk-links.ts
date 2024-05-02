d367e94dce5a01ff034d07459abd8660d91f6780
Julien Vannier <julien.vannier66@gmail.com>
2024-05-02 10:31:25 +0200

    Remove influencer network modal component

--- addon/resources/helpdesk-links.ts
+++ addon/resources/helpdesk-links.ts
@@ -1,3 +1,1 @@
 export const GLOBAL_SUPPORT_LINK = 'https://help.upfluence.co/en/';
-export const COMMUNITY_HELP_LINK =
-  'https://help.upfluence.co/en/articles/7005414-community-size-and-influencer-community-how-does-it-work';
