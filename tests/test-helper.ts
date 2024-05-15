6472b2ced628b7df12c96fc42efa44379eb7d392
Maximilien B <maximilien.brunelle@gmail.com>
2024-05-15 08:47:18 +0200

    Enable sonarqube on ember-upf-utils

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -6,8 +6,14 @@ import { start } from 'ember-qunit';
 import setupSinon from 'ember-sinon-qunit';
 import QUnit from 'qunit';
 import { setup } from 'qunit-dom';
+import { forceModulesToBeLoaded, sendCoverage } from 'ember-cli-code-coverage/test-support';
 
 setup(QUnit.assert);
+QUnit.done(async function () {
+  forceModulesToBeLoaded();
+  await sendCoverage();
+});
+
 setApplication(Application.create(config.APP));
 setupSinon();
 
