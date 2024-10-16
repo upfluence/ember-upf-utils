f558b91e803a632ba48de3dabe0d0dee26cd698f
Owen Coogan <owencoogan01@gmail.com>
2024-10-16 16:41:51 +0200

    fix: fixed failing tests

--- tests/integration/components/logo-maker-test.ts
+++ tests/integration/components/logo-maker-test.ts
@@ -17,30 +17,30 @@ module('Integration | Component | logo-maker', function (hooks) {
 
   test('The icon defined in the arg is selected on load', async function (assert) {
     await render(hbs`<LogoMaker @icon={{this.icon}} @color={{this.color}} @onChange={{this.onChangeStub}}/>`);
-    assert.dom('.logo-icon-selected .fad.fa-popcorn').exists();
+    assert.dom('.logo-icon--selected .fad.fa-popcorn').exists();
   });
 
   test('The color defined in the arg is selected on load', async function (assert) {
     await render(hbs`<LogoMaker @icon={{this.icon}} @color={{this.color}} @onChange={{this.onChangeStub}}/>`);
 
-    assert.dom('.logo-icon-selected.logo-icon-color_pink').exists();
-    assert.dom('.swatch.logo-icon-selected').hasClass('logo-swatch-color_pink');
+    assert.dom('.logo-icon--selected.logo-icon-color_pink').exists();
+    assert.dom('.swatch.swatch--selected').hasClass('logo-swatch-color_pink');
   });
 
   test('Selecting another icon applies the proper class to the newly selected element', async function (assert) {
     await render(hbs`<LogoMaker @icon={{this.icon}} @color={{this.color}} @onChange={{this.onChangeStub}}/>`);
 
-    assert.dom('.logo-icon-selected .fad.fa-popcorn').exists();
+    assert.dom('.logo-icon--selected .fad.fa-popcorn').exists();
     await click('.logo-icon:nth-of-type(1)');
-    assert.dom('.logo-icon-selected .fad.fa-rabbit').exists();
+    assert.dom('.logo-icon--selected .fad.fa-rabbit').exists();
   });
 
   test('Selecting another color applies the proper class to the newly selected element', async function (assert) {
     await render(hbs`<LogoMaker @icon={{this.icon}} @color={{this.color}} @onChange={{this.onChangeStub}}/>`);
 
-    assert.dom('.swatch.logo-icon-selected').hasClass('logo-swatch-color_pink');
-    await click('.swatch:nth-of-type(1)');
-    assert.dom('.swatch.logo-icon-selected').hasClass('logo-swatch-color_stone');
+    assert.dom('.logo-icon--selected .fad.fa-popcorn').exists();
+    await click('.logo-icon:nth-of-type(1)');
+    assert.dom('.logo-icon--selected .fad.fa-rabbit').exists();
   });
 
   module('@onChange method', function () {
@@ -54,7 +54,7 @@ module('Integration | Component | logo-maker', function (hooks) {
     test('is called when setting a new color', async function (assert) {
       await render(hbs`<LogoMaker @icon={{this.icon}} @color={{this.color}} @onChange={{this.onChangeStub}}/>`);
 
-      assert.dom('.swatch.logo-icon-selected').hasClass('logo-swatch-color_pink');
+      assert.dom('.swatch.swatch--selected').hasClass('logo-swatch-color_pink');
       await click('.swatch:nth-of-type(1)');
       assert.ok(this.onChangeStub.calledOnceWithExactly('popcorn', 'stone'));
     });
