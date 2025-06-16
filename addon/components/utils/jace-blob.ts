a506c923460a5696a089bbfaea6833da9b35f451
Owen Coogan <owencoogan01@gmail.com>
2025-06-16 17:44:56 +0200

    fix: fixes post review

--- addon/components/utils/jace-blob.ts
+++ addon/components/utils/jace-blob.ts
@@ -1,13 +1,9 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-
 interface JaceBlobArgs {
   loading: boolean;
 }
 
 export default class JaceBlob extends Component<JaceBlobArgs> {
-  @tracked displayedLoading = this.args.loading;
-
   get loadingClass(): string {
     return this.args.loading ? 'fade-in' : 'fade-out';
   }
