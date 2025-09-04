b8df2858465428e2b2943637040c50be43fe2c62
Olympe Lespagnon <olympe.lespagnon@upfluence.com>
2025-09-04 10:19:56 +0200

    Use normal function notation

--- addon/helpers/format-number.js
+++ addon/helpers/format-number.js
@@ -3,7 +3,7 @@ import { helper as buildHelper } from '@ember/component/helper';
 export const PREVENT_COMPACT_NOTATION_BELOW = 1000;
 export const ROUND_TO_INTEGER_ABOVE = 100;
 
-const _getFormatter = function (number) {
+function getFormatter(number) {
   const absoluteValue = Math.abs(number);
 
   const options =
@@ -14,17 +14,16 @@ const _getFormatter = function (number) {
       : { minimumFractionDigits: 0, maximumFractionDigits: 2 };
 
   return Intl.NumberFormat(['en-EN', 'fr-FR'], options);
-};
+}
 
-const formatNumber = function formatNumber(params) {
+export function formatNumber(params) {
   const number = params[0];
 
   if (number === null || number === undefined || isNaN(number)) {
     return '-';
   }
 
-  return _getFormatter(number).format(number);
-};
+  return getFormatter(number).format(number);
+}
 
-export { formatNumber };
 export default buildHelper(formatNumber);
