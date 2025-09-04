00b2372ed671286717fdf934b4c8e180c9c05848
Olympe Lespagnon <olympe.lespagnon@upfluence.com>
2025-09-04 11:06:12 +0200

    Migrate to TS

--- addon/helpers/format-number.ts
+++ addon/helpers/format-number.ts
@@ -3,20 +3,20 @@ import { helper as buildHelper } from '@ember/component/helper';
 export const PREVENT_COMPACT_NOTATION_BELOW = 1000;
 export const ROUND_TO_INTEGER_ABOVE = 100;
 
-function getFormatter(number) {
-  const absoluteValue = Math.abs(number);
+function getFormatter(number: number): Intl.NumberFormat {
+  const absoluteValue: number = Math.abs(number);
 
-  const options =
+  const options: Intl.NumberFormatOptions =
     absoluteValue >= PREVENT_COMPACT_NOTATION_BELOW
       ? { minimumFractionDigits: 0, maximumFractionDigits: 1, notation: 'compact' }
       : absoluteValue >= ROUND_TO_INTEGER_ABOVE
       ? { minimumFractionDigits: 0, maximumFractionDigits: 0 }
       : { minimumFractionDigits: 0, maximumFractionDigits: 2 };
 
-  return Intl.NumberFormat(['en-EN', 'fr-FR'], options);
+  return new Intl.NumberFormat(['en-EN', 'fr-FR'], options);
 }
 
-export function formatNumber(params) {
+export function formatNumber(params: Array<number | null | undefined>): string {
   const number = params[0];
 
   if (number === null || number === undefined || isNaN(number)) {
