d098da035ef5352cbd6e97c8a062fd728e56c6c2
Antoine Prentout <antoine.prentout@upfluence.com>
2025-09-15 16:09:40 +0200

    [WIP] Remove computed deprecations types/user notifications/service

--- addon/types/user.js
+++ addon/types/user.js
@@ -1,12 +1,12 @@
 import EmberObject, { computed } from '@ember/object';
 
 export default EmberObject.extend({
-  fullName: computed('first_name', 'last_name', function () {
+  get fullName(): string {
     let { first_name, last_name } = this;
     if (first_name || last_name) {
       return `${first_name} ${last_name}`;
     }
 
     return 'Anonymous User';
-  })
+  }
 });
