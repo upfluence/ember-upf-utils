4563da81d08a4d44720438828b5982ee70b4740f
Maximilien B <maximilien.brunelle@gmail.com>
2026-01-22 09:50:50 +0100

    Updated: improve URL regex pattern recognition and added test cases

--- addon/utils/url-regex.ts
+++ addon/utils/url-regex.ts
@@ -1,1 +1,2 @@
-export const URL_REGEX = /^(((https?:\/\/)?(www)?)?([a-zA-Z0-9-]{2,}(\.[a-zA-Z0-9-]{1,})(\.[a-zA-Z0-9\/]{2,})?(\/[^\s]{0,})?))$/;
+export const URL_REGEX =
+  /^(https?:\/\/)?((www\.([a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)|((?!www)(?!\d+\.\d+\.\d+\.\d+)[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)(\/[^\s]*)?(#[^\s]*)?$/;
