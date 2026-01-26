030f188060ea3ca59a2a03a0ccbd0f960fe7c27d
Julien Vannier <julien.vannier66@gmail.com>
2026-01-26 17:06:24 +0100

    Add tests the setup-autocomplete modifier

--- addon/modifiers/setup-autocomplete.ts
+++ addon/modifiers/setup-autocomplete.ts
@@ -2,11 +2,12 @@ import Modifier, { type ArgsFor, type PositionalArgs, type NamedArgs } from 'emb
 import { registerDestructor } from '@ember/destroyable';
 import { assert } from '@ember/debug';
 import { getOwner } from '@ember/application';
-
 import { isTesting } from '@embroider/macros';
 
 import { Loader } from '@googlemaps/js-api-loader';
-import { parseAddressComponents, type AutocompletionAddress } from '../utils/address-parser';
+
+import { parseAddressComponents, type AutocompletionAddress } from '@upfluence/ember-upf-utils/utils/address-parser';
+import { MockLoader } from '@upfluence/ember-upf-utils/utils/google-maps-mock';
 
 type GooglePlaceResult = google.maps.places.PlaceResult;
 type GoogleAutocomplete = google.maps.places.Autocomplete;
@@ -17,6 +18,7 @@ interface SetupAutocompleteSignature {
   Args: {
     Named: {
       callback(result: AutocompletionAddress): void;
+      loader?: Loader;
     };
   };
 }
@@ -29,9 +31,18 @@ const AUTOCOMPLETE_OPTIONS: GoogleAutocompleteOptions = {
 };
 
 function cleanup(instance: SetupAutocompleteModifier): void {
+  if (instance.createdWrapper && instance.targetInput && instance.targetElement) {
+    const parent = instance.targetElement.parentNode;
+    if (parent && instance.targetElement.contains(instance.targetInput)) {
+      parent.insertBefore(instance.targetInput, instance.targetElement);
+      instance.targetElement?.remove();
+    }
+  }
+
   instance.targetElement = null;
   instance.targetInput = null;
   instance.result = null;
+  instance.createdWrapper = false;
   document.querySelector('.pac-container')?.remove();
 }
 
@@ -39,6 +50,7 @@ export default class SetupAutocompleteModifier extends Modifier<SetupAutocomplet
   targetElement: HTMLElement | null = null;
   targetInput: HTMLInputElement | null = null;
   result: AutocompletionAddress | null = null;
+  createdWrapper: boolean = false;
 
   private callback: ((result: AutocompletionAddress) => void) | null = null;
 
@@ -50,15 +62,18 @@ export default class SetupAutocompleteModifier extends Modifier<SetupAutocomplet
   modify(
     element: HTMLElement,
     _: PositionalArgs<SetupAutocompleteSignature>,
-    { callback }: NamedArgs<SetupAutocompleteSignature>
+    { callback, loader }: NamedArgs<SetupAutocompleteSignature>
   ): void {
     const input: HTMLInputElement | null = this.getInputElement(element);
     if (!input) return;
 
     this.targetInput = input;
     this.callback = callback;
-    this.setupTargetElement(element, input);
-    this.setupAutoComplete();
+
+    if (!this.targetElement) {
+      this.setupTargetElement(element, input);
+      this.setupAutoComplete(loader);
+    }
   }
 
   private setupTargetElement(element: HTMLElement, input: HTMLInputElement): void {
@@ -71,14 +86,20 @@ export default class SetupAutocompleteModifier extends Modifier<SetupAutocomplet
   }
 
   private createWrapperForInput(input: HTMLInputElement): HTMLElement {
-    const wrapper = document.createElement('div');
-    wrapper.classList.add(AUTOCOMPLETE_CONTAINER_CLASS);
-
     const parentNode = input.parentNode;
     assert('[modifier][setup-autocomplete] Input element must have a parent node', parentNode !== null);
 
+    if (parentNode instanceof HTMLElement && parentNode.classList.contains(AUTOCOMPLETE_CONTAINER_CLASS)) {
+      return parentNode;
+    }
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add(AUTOCOMPLETE_CONTAINER_CLASS);
+
     parentNode.insertBefore(wrapper, input);
     wrapper.appendChild(input);
+
+    this.createdWrapper = true;
 
     return wrapper;
   }
@@ -98,15 +119,15 @@ export default class SetupAutocompleteModifier extends Modifier<SetupAutocomplet
     return element.tagName === 'INPUT' && (element as HTMLInputElement).type === 'text';
   }
 
-  private async setupAutoComplete(): Promise<void> {
-    if (isTesting()) return;
+  private async setupAutoComplete(loader?: Loader): Promise<void> {
+    const loaderInstance = isTesting()
+      ? loader ?? new MockLoader({ apiKey: 'test-key' })
+      : new Loader({
+          apiKey: getOwner(this).resolveRegistration('config:environment').google_map_api_key,
+          version: 'weekly'
+        });
 
-    const loader = new Loader({
-      apiKey: getOwner(this).resolveRegistration('config:environment').google_map_api_key,
-      version: 'weekly'
-    });
-
-    loader.importLibrary('places').then(({ Autocomplete }) => {
+    loaderInstance.importLibrary('places').then(({ Autocomplete }) => {
       this.initializeAutocomplete(Autocomplete);
     });
   }
