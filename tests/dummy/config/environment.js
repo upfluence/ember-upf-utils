29e8c8cd56c6270c50a557eb753aa809bfc1e899
Julien Vannier <julien.vannier66@gmail.com>
2026-02-03 16:32:54 +0100

    Add new autocomplete-handler service & tests

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -42,6 +42,7 @@ module.exports = function (environment) {
 
     ENV.APP.rootElement = '#ember-testing';
     ENV.APP.autoboot = false;
+    ENV.google_map_api_key = 'foobar';
   }
 
   if (environment === 'production') {
