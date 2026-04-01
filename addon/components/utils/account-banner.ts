beb66c4d2c046b24c95aadc0d8d5450250cf4ca3
Julien Vannier <julien.vannier66@gmail.com>
2026-04-01 18:09:41 +0200

    Add form logic

--- addon/components/utils/account-banner.ts
+++ addon/components/utils/account-banner.ts
@@ -2,6 +2,10 @@ import { action } from '@ember/object';
 import { isBlank } from '@ember/utils';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { IntlService } from 'ember-intl';
+
+import { Feedback, FormInstance } from '@upfluence/oss-components/services/form-manager';
 
 type SkinType = 'success' | 'error' | 'warning';
 
@@ -27,10 +31,18 @@ interface UtilsAccountBannerArgs {
 
   canSelectItem?: boolean;
   selectableItems?: any[];
+  selectedAccount?: any;
+
+  onFormSetup?(form: FormInstance): void;
 }
 
 export default class extends Component<UtilsAccountBannerArgs> {
+  @service declare intl: IntlService;
+
+  declare formInstance: FormInstance;
+
   @tracked displaySelectableItems: boolean = false;
+  @tracked isErrored: boolean = false;
 
   get disabledClass(): string {
     return this.args.disabled ? 'account-banner--disabled' : '';
@@ -41,7 +53,7 @@ export default class extends Component<UtilsAccountBannerArgs> {
   }
 
   get borderColorClass(): string {
-    if (this.args.skin) return `account-banner--${this.args.skin}`;
+    if (this.args.skin || this.isErrored) return `account-banner--${this.args.skin}`;
     return '';
   }
 
@@ -51,6 +63,14 @@ export default class extends Component<UtilsAccountBannerArgs> {
 
   get canSelectItem(): boolean {
     return ((this.args.selectableItems || []).length > 1 && this.args.canSelectItem) ?? false;
+  }
+
+  noop(): void {}
+
+  @action
+  onFormSetup(form: FormInstance): void {
+    this.formInstance = form;
+    this.args.onFormSetup?.(form);
   }
 
   @action
@@ -64,5 +84,19 @@ export default class extends Component<UtilsAccountBannerArgs> {
   @action
   closeSelectionDropdown(): void {
     this.displaySelectableItems = false;
+  }
+
+  @action
+  validateAccountSelection(): Feedback | undefined {
+    if (!this.args.selectedAccount) {
+      this.isErrored = true;
+      return {
+        kind: 'blank',
+        message: { type: 'error', value: this.intl.t('oss-components.forms.errors.required') }
+      };
+    }
+
+    this.isErrored = false;
+    return undefined;
   }
 }
