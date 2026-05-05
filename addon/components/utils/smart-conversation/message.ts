fc8f8f9aa4f24bcaf5f606f45426b3ba8fae3120
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2026-05-05 13:12:10 +0200

    add support for SmartConversation::Message component

---
+++ addon/components/utils/smart-conversation/message.ts
@@ -0,0 +1,40 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+import moment from 'moment';
+
+interface UtilsSmartConversationMessageComponentSignature {
+  type: 'smart_reply' | 'user_prompt';
+  value: string;
+  timestamp: number;
+}
+
+export default class UtilsSmartConversationMessageComponent extends Component<UtilsSmartConversationMessageComponentSignature> {
+  @tracked collapsed: boolean = true;
+
+  get collapsible(): boolean {
+    return this.args.type === 'smart_reply';
+  }
+
+  get computedClasses(): string {
+    const classes = ['smart-conversation-message', `smart-conversation-message--${this.args.type}`];
+
+    if (this.collapsible && this.collapsed) {
+      classes.push('smart-conversation-message--collapsed');
+    }
+
+    return classes.join(' ');
+  }
+
+  get formattedTimestamp(): string {
+    return moment(this.args.timestamp).format('DD/MM/YYYY, HH:mm');
+  }
+
+  toggleCollapsed = (event: MouseEvent) => {
+    event.stopPropagation();
+
+    if (!this.collapsible) return;
+
+    this.collapsed = !this.collapsed;
+  };
+}
