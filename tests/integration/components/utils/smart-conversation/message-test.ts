a19c679c33827a3c4d373e3b61f63561fd5fd0f6
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2026-06-03 17:30:46 +0200

    reapply prettier --write

--- tests/integration/components/utils/smart-conversation/message-test.ts
+++ tests/integration/components/utils/smart-conversation/message-test.ts
@@ -1,8 +1,9 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
 import { click, render } from '@ember/test-helpers';
+
 import { hbs } from 'ember-cli-htmlbars';
+import { setupRenderingTest } from 'ember-qunit';
 import moment from 'moment';
+import { module, test } from 'qunit';
 
 const LONG_VALUE = 'Lorem ipsum dolor sit amet, '.repeat(100);
 const SHORT_VALUE = 'Short message';
